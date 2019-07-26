ac39a3171526bbdaf75ff8a20c8491638c3cd23a
Miguel Mota <hello@miguelmota.com>
2019-07-25 18:07:55 -0700

    error handling

--- purge.rb
+++ purge.rb
@@ -1,25 +1,23 @@
 require 'dotenv/load'
 require 'twitter'
 
-$client = Twitter::REST::Client.new do |config|
+@client = Twitter::REST::Client.new do |config|
 	config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
 	config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
 	config.access_token = ENV['TWITTER_ACCESS_TOKEN_KEY']
 	config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
-$purge_list = []
-
 def check(friend_id)
   puts "checking user: #{friend_id}"
 
-  timeline = $client.user_timeline(friend_id, { :include_rts => false, :count => 1 })
+  timeline = @client.user_timeline(friend_id, { :include_rts => false, :count => 1 })
 
   if timeline.count == 0
-    purge_list << friend_id
+    return unfollow(friend_id)
   end
 
-  status = $client.status(timeline[0].id)
+  status = @client.status(timeline[0].id)
 
   date = status.created_at.to_date
 
@@ -27,9 +25,25 @@ def check(friend_id)
   thirty_days_ago = (now - 30)
 
   if date < thirty_days_ago
-    puts "added to list: #{friend_id}"
-    $purge_list << friend_id
+    unfollow(friend_id)
   end
+end
+
+def following(username)
+  @client.friend_ids(username).to_a
+rescue Twitter::Error::TooManyRequests => error
+  p error
+  sleep error.rate_limit.reset_in
+  retry
+end
+
+def unfollow(friend_id)
+  @client.unfollow(friend_id)
+  puts "unfollowed: #{friend_id}"
+rescue Twitter::Error::TooManyRequests => error
+  p error
+  sleep error.rate_limit.reset_in
+  retry
 end
 
 if ARGV.empty?
@@ -40,22 +54,9 @@ end
 username = ARGV[0]
 puts "user: #{username}"
 
-following_ids = $client.friend_ids(username)
+following_ids = following(username)
 puts "following count: #{following_ids.count}"
 
-following_ids.each { |friend_id|
+following_ids.each do |friend_id|
   check(friend_id)
-
-  # NOTE: sleep is to prevent rate limit errors
-  sleep(2)
-}
-
-puts "purging #{$purge_list.count} accounts"
-
-$purge_list.each { |friend_id|
-  $client.unfollow(friend_id)
-  puts "unfollowed: #{friend_id}"
-
-  # NOTE: sleep is to prevent rate limit errors
-  sleep(2)
-}
+end
